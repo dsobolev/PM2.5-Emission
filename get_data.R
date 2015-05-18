2ea54b283f464dadc194ca04c550a84bb016860c
dsobolev <dsobolev@dataart.com>
2015-05-18 19:00:21 +0300

    First plot

---
+++ get_data.R
@@ -0,0 +1,3 @@
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", "data.zip")
+unzip("data.zip")
+file.remove("data.zip")
---
+++ plot1.R
@@ -0,0 +1,11 @@
+pmSummary <- readRDS("summarySCC_PM25.rds")
+
+library(dplyr)
+dtPlot <- pmSummary %>% select(Emissions, year) %>% group_by(year) %>% summarize(total = sum(Emissions))
+
+png("plot1.png")
+
+plot(dtPlot, main = "PM2.5 Emission", ylab = "Total PM2.5 Emission", col = "red", pch = 19)
+with(dtPlot, abline(lm(total ~ year), lty = "18"))
+
+dev.off()
