e272f0a5d247fb36eb008945ec568c90bfeb3b15
dsobolev <dsobolev@dataart.com>
2015-05-20 19:37:43 +0300

    Plot #3

---
+++ plot3.R
@@ -0,0 +1,24 @@
+pmSummary <- readRDS("summarySCC_PM25.rds")
+
+library(dplyr)
+library(ggplot2)
+
+#Prepare data
+dtPlot <- pmSummary %>% 
+  filter(fips == "24510") %>% 
+  select(Emissions, year, type) %>%
+  mutate(type = as.factor(type)) %>%
+  group_by(year, type) %>%
+  summarize(emission = sum(Emissions))
+
+#Build plotting object
+plotObj <- qplot(year, emission, data = dtPlot, facets = . ~ type, geom = c("point", "smooth"), method = "lm") + 
+  ggtitle("PM2.5 emissons by Types") + 
+  theme(plot.title = element_text(face = "bold")) + 
+  ylab("PM2.5 Emission") + 
+  scale_x_continuous(breaks = c(2002, 2006))
+
+#print results
+png("plot3.png")
+print(plotObj)
+dev.off()
